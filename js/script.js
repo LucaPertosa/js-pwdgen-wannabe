3b5958a0fc78e9c62c4ca03298832dd4578b3b53
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-07 13:57:06 +0100

    Add: ragionamento logico js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert('Ciao!')
+// Generatore di password insicurissimo
+
+// Numero generato a fine password
+
+// Domandare all'utente il suo nome
+
+// Domandare all'utente il suo cognome
+
+// Domandare all'utente il suo colore preferito
+
+// Compongo la password
+
+// Preparo il messaggio con il risultato
+
+// Stampo il messaggio
